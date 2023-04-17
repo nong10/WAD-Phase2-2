1ad7c451d6095837513b3c6ddbba1bcbe923dddb
nong10 <nong10w@outlook.com>
2023-04-17 17:36:54 +0200

    git init

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Recipe Sharing Homepage</title>
+    <link rel="stylesheet" href="./stylesheets/main.css">
+    <link rel="stylesheet" href="./stylesheets/index.css">
+    <link rel="stylesheet" href="./stylesheets/header.css">
+</head>
+<body>
+    <div id="header">
+        <a href="./index.html">Da-Recipes</a>
+        <a href="./pages/profile/favorite.html">Favorite</a>
+        <a href="./pages/profile/profile.html">Profile </a>
+        <input type="text" placeholder="Search for recipes ...">
+        <a href="./pages/account/login.html">Login</a>
+    </div>
+
+    <div class="recipe-preview">
+        <div class="p-container">
+            <a href="./pages/recipes/recipe_detail.html">
+                <h2>Ragu alla Bolognese</h2>
+            </a>
+            <p>
+                Succulents are a group of plants that store water in their leaves, 
+                stems, and roots, allowing them to survive in arid and dry climates.
+                Some examples of succulent plants that produce flowers include 
+                cacti, aloes, agaves, and echeverias.
+            </p>
+        </div>
+        <div class="img-container">
+            <img src="./images/succulent-flower.JPG" alt="flower">
+        </div>
+    </div>
+
+    <div class="recipe-preview">
+        <div class="p-container">
+            <a href="./pages/recipes/recipe_detail.html">
+                <h2>Ragu alla Bolognese</h2>
+            </a>
+            <p>
+                Succulents are a group of plants that store water in their leaves, 
+                stems, and roots, allowing them to survive in arid and dry climates.
+                Some examples of succulent plants that produce flowers include 
+                cacti, aloes, agaves, and echeverias.
+            </p>
+        </div>
+        <div class="img-container">
+            <img src="./images/succulent-flower.JPG" alt="flower">
+        </div>
+    </div>
+    
+    
+</body>
+</html>
